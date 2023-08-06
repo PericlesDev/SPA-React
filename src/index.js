62df2bc6b5d74f52ec3237b51e24ea138781a731
unknown <PericlesDosSantosRamos@hotmail.com>
2023-08-06 17:50:25 -0300

    Interface main da SPA

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import home from "./pages/home/main.js";
+import about from "./pages/about/main.js";
+import contacts from "./pages/contacts/main.js";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const main = document.querySelector("#root");
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const init = () => {
+   window.addEventListener("hashchange", () => {
+      main.innerHTML = "";
+      switch(window.location.hash){
+         case " ":
+         main.appendChild(home());
+         break;
+         case "#about":
+            main.appendChild(about());
+         break;
+         case "#contacts":
+            main.appendChild(contacts());
+         break;
+         default:
+         main.appendChild(home());
+      }
+})
+}
+
+window.addEventListener("load", () => {
+   main.appendChild(home());
+   init();
+})
